90de75b4e1ad07847ef13d172587f6b17af363de
juan.posada4@utp.edu.co <juan.posada4@gmail.com>
2026-02-08 23:01:15 -0500

    Modificación del diseño para algunos html. Se agrega post relevantes del CORE en dos secciones y se mejora la visualizacion para ponerse en contacto

--- src/app/pages/campanas/campanas.component.ts
+++ src/app/pages/campanas/campanas.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FirmaCounterService } from '../../services/firma-counter.service';
 
 @Component({
   selector: 'app-campanas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './campanas.component.html',
   styleUrls: ['./campanas.component.css']
 })
-export class CampanasComponent {}
+export class CampanasComponent implements OnInit {
+  firmasTotales: number = 0;
+  metaFirmas: number = 10000;
+
+  constructor(private firmaCounterService: FirmaCounterService) {}
+
+  ngOnInit() {
+    this.firmaCounterService.obtenerContador().subscribe(contador => {
+      this.firmasTotales = contador;
+    });
+  }
+
+  get porcentajeProgreso(): number {
+    return Math.min((this.firmasTotales / this.metaFirmas) * 100, 100);
+  }
+}
